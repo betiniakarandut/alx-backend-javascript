102a5c34209e7bf927cc7e07c0898c49b84e1f43
betiniakarandut <betiniakarandut@gmail.com>
2023-08-01 12:35:25 +0100

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+import getListStudents from './0-get_list_students.js';
+
+export default function getListStudentIds(arr) {
+	if (!Array.isArray(arr))
+		return []
+	const arr2 = arr.map(i => i.id);
+	return arr2
+}
+
+
+// console.log(getListStudentIds("hello"));
+// console.log(getListStudentIds(getListStudents));
