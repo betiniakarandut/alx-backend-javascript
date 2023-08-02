7d790133eb43f3dc0b3e3c016cef1748338f5db5
betiniakarandut <betiniakarandut@gmail.com>
2023-08-02 02:29:34 +0100

    Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,14 @@
+import getListStudents from './0-get_list_students.js';
+
+export default function getStudentIdsSum(students) {
+	const reduced = students
+		.map((value) => value.id)
+		.reduce((acc, value) => {
+			return acc + value;
+		}, 0);
+	return reduced;
+}
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+console.log(value);
