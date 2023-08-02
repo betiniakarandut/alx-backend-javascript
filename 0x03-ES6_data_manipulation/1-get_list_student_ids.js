4d26b08a6779ceb907b85b639ece4d5eb3c84336
betiniakarandut <betiniakarandut@gmail.com>
2023-08-02 03:08:39 +0100

    More objects

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-import getListStudents from './0-get_list_students.js';
+// import getListStudents from './0-get_list_students.js';
 
 export default function getListStudentIds(arr) {
 	if (!Array.isArray(arr))
