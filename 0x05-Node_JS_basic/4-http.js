188071495cda1c8c50d14cc463d2ad41033e88c1
betiniakarandut <betiniakarandut@gmail.com>
2023-10-17 04:18:59 +0100

    Updated server controllers

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,25 +1,13 @@
 const http = require('http');
 
-/**
- * creates a small HTTP server using the http module
- * listening on port 1245
- */
-
 const PORT = 1245;
-const HOST = 'localhost';
-const app = http.createServer();
 
-app.on('request', (_, response) => {
-  const responseText = 'Hello Holberton School!';
-
-  response.setHeader('Content-Type', 'text/plain');
-  response.setHeader('Content-Length', responseText.length);
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  response.write(Buffer.from(responseText));
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-});
+app.listen(PORT, () => {});
 
 module.exports = app;
